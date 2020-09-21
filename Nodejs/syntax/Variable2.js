df60d8a8145bf82caa5d81669b6b0c7fb59d0eb1
김태우 <taewoo@Taewoo.local>
2020-09-21 10:22:52 +0900

    2020-09-20

---
+++ Nodejs/syntax/Variable2.js
@@ -0,0 +1,3 @@
+var name = 'Taewoo';
+var letter = 'Dear ' + name+ ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, egoing quis nostrud exercitation ullamco laboris nisi ut ' + name+ ' aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ' + name+ ' Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+console.log(letter);
