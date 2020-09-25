d9b84bb61c905667dfe77b91cb4c508220e6be58
김태우 <taewoo@Taewoo.local>
2020-09-25 19:31:44 +0900

    2020_09_25

--- Nodejs/main.js
+++ Nodejs/main.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+
 function templateHTML(title,list,body){
   return `
   <!doctype html>
@@ -19,7 +20,7 @@ function templateHTML(title,list,body){
 }
 function templateList(filelist){
   var list = "<ul>";
-  let i = 0;
+  var i = 0;
   while (i < filelist.length) {
     list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
     i++;
@@ -31,13 +32,12 @@ var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
-  
   if (pathname === "/") {
     if (queryData.id === undefined) {
       fs.readdir("./data", function (error, filelist) {
         var title = "Welcome";
         var description = "Hello, Node.js";
-        var list = templateList(filelist)
+        var list = templateList(filelist);
         var template = templateHTML(title,list, `<h2>${title}</h2>${description}`)
         response.writeHead(200);
         response.end(template);
@@ -46,7 +46,7 @@ var app = http.createServer(function (request, response) {
       fs.readdir('./data', function (error, filelist) {
         fs.readFile(`data/${queryData.id}`, "utf8", function (err,description) {
           var title = queryData.id;
-            var list = templateList(filelist)
+            var list = templateList(filelist);
           var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
